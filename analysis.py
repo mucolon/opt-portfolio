497450c13769ae535260091de2f2c0d1a48bcc87
Marco U. Colón <marcocolon@live.com>
2020-11-16 05:48:41 -0800

    added m1 portfolio analysis

--- analysis.py
+++ analysis.py
@@ -14,6 +14,19 @@ def f2p(float_):
     return str(round(100*float_, 2))+"%"
 
 
+def dollar2f(dollar):
+    return float(dollar.strip("$"))
+
+
+def f2dollar(float_):
+    dollar = "$" + str(round(float_, 2))
+    length = len(dollar)
+    decimal = dollar.index(".")
+    if (length - decimal) == 2:
+        dollar = dollar + "0"
+    return dollar
+
+
 def insert_position(position, list1, list2):
     return list1[:position] + list2 + list1[position:]
 
@@ -71,9 +84,9 @@ class Performance:
         for _ in range(len(del_columns)):
             del self.df[del_columns[_]]
         # start analysis
-        self.analysis()
+        self.analyze()
 
-    def analysis(self):
+    def analyze(self):
         # add 3y data columns to dataframe
         col_3y_perf = self.df.columns.get_loc("3Y Perf")
         self.df.insert(col_3y_perf+1, "3Y Div Perf", np.nan)
@@ -215,16 +228,17 @@ class Dividends:
         for _ in range(len(del_columns)):
             del self.df[del_columns[_]]
         # start analysis
-        self.analysis()
+        self.analyze()
 
-    def analysis(self):
-        # column index for 4y average yield
+    def analyze(self):
+        # column index for yield
         col_fwd_yield = self.df.columns.get_loc("Yield FWD")
         col_yields = [col_fwd_yield-1, col_fwd_yield]
         self.col_mod = np.array(col_yields)
         self.p2f_data(self.col_mod)
         ave_yield = self.df.iloc[:, col_yields].mean(axis=1)
-        self.df.insert(col_fwd_yield+1, "2Y Ave Yield", ave_yield)
+        fwd_yield = 2*ave_yield - self.df.iloc[:, col_fwd_yield-1]
+        self.df.insert(col_fwd_yield+1, "Yield", fwd_yield)
 
         # add average dividend growth column
         col_5y_growth = self.df.columns.get_loc("Div Growth 5Y")
@@ -267,9 +281,122 @@ class Dividends:
 
 class Holdings:
 
-    def __init__(self, path):
+    def __init__(self, path, div_df, del_columns):
         self.path = path
         self.df = pd.read_csv(path)
+        self.df.set_index("Ticker", inplace=True)
+        self.df.rename_axis("Symbols", inplace=True)
+        self.df["Yield"] = div_df["Yield"]
+        self.df["Div Rate TTM"] = div_df["Div Rate TTM"]
+        self.df["Div Rate FWD"] = div_df["Div Rate FWD"]
+        self.df.rename(columns={"Maintenance": "Cost Basis",
+                                "Avg. Price": "Ave Price"}, inplace=True)
+        # delete irrelevant columns
+        for _ in range(len(del_columns)):
+            del self.df[del_columns[_]]
+        # start analysis
+        self.analyze()
+
+    def analyze(self):
+        # dividend rate ttm column index
+        col_div_ttm = self.df.columns.get_loc("Div Rate TTM")
+        # average price column index
+        col_ave_price = self.df.columns.get_loc("Ave Price")
+        col_cost_basis = self.df.columns.get_loc("Cost Basis")
+        # string dollar column indexes
+        self.col_dollar = [col_div_ttm, col_div_ttm+1,
+                           col_ave_price, col_ave_price+1]
+        # convert string dollar to float
+        self.dollar2f_data(self.col_dollar)
+        # column indexes for dividend rate ttm & fwd
+        col_div_rates = [col_div_ttm, col_div_ttm+1]
+
+        # average dividend rate of ttm & fwd
+        ave_rate = self.df.iloc[:, col_div_rates].mean(axis=1)
+        # fwd dividend rate
+        fwd_rate = 2*ave_rate - self.df.iloc[:, col_div_ttm]
+        self.df["Div Rate"] = fwd_rate
+        # usable fwd dividend rate column index
+        col_div = self.df.columns.get_loc("Div Rate")
+
+        # annual dividends
+        div_annual = fwd_rate*self.df["Shares"]
+        self.df["Annual Div"] = div_annual
+        self.portfolio_div_annual = self.df["Annual Div"].sum()
+        col_annual = self.df.columns.get_loc("Annual Div")
+
+        # cost basis allocations
+        total_invest = self.df["Cost Basis"].sum()
+        allocation = self.df["Cost Basis"] / total_invest
+        self.df["Allocation"] = allocation
+        col_all = self.df.columns.get_loc("Allocation")
+
+        # yield on cost
+        yoc = fwd_rate / self.df["Ave Price"]
+        self.df["YoC"] = yoc
+        col_yoc = self.df.columns.get_loc("YoC")
+        col_yield = self.df.columns.get_loc("Yield")
+
+        # average yield
+        self.ave_yield = sum(allocation * self.df["Yield"])
+        # average yoc
+        self.ave_yoc = sum(allocation * yoc)
+
+        # final dataframe
+        self.df = self.df[["Allocation", "Shares", "Ave Price", "Cost Basis",
+                           "Annual Div", "Yield", "YoC"]]
+        self.col_dollar_fin = [2, 3, 4]
+        self.col_percent_fin = [0, 5, 6]
+
+    def p2f_data(self, column_list):
+        for j in range(len(column_list)):
+            filt = (self.df.iloc[:, column_list[j]].str.contains("%"))
+            edit = self.df.loc[filt,
+                               self.df.columns[column_list[j]]].apply(p2f)
+            self.df.iloc[:, column_list[j]] = edit
+
+    def f2p_data(self, column_list):
+        for j in range(len(column_list)):
+            filt = (self.df.iloc[:, column_list[j]].apply(np.isnan) != True)
+            edit = self.df.loc[filt,
+                               self.df.columns[column_list[j]]].apply(f2p)
+            self.df.iloc[:, column_list[j]] = edit
+
+    def dollar2f_data(self, column_list):
+        for j in range(len(column_list)):
+            filt = (~self.df.iloc[:, column_list[j]].str.contains("-"))
+            edit = self.df.loc[filt,
+                               self.df.columns[column_list[j]]].apply(dollar2f)
+            self.df.iloc[:, column_list[j]] = edit
+
+    def f2dollar_data(self, column_list):
+        for j in range(len(column_list)):
+            filt = (self.df.iloc[:, column_list[j]].apply(np.isnan) != True)
+            edit = self.df.loc[filt,
+                               self.df.columns[column_list[j]]].apply(f2dollar)
+            self.df.iloc[:, column_list[j]] = edit
+
+    def cleanup(self):
+        self.f2dollar_data(self.col_dollar_fin)
+        self.f2p_data(self.col_percent_fin)
+
+    def present(self):
+        self.portfolio_div_annual = f2dollar(self.portfolio_div_annual)
+        self.ave_yield = f2p(self.ave_yield)
+        self.ave_yoc = f2p(self.ave_yoc)
+        print(self.df)
+        print("\n===========\t===========\t===========\t===========\t===========\t\n")
+        print("Annual Portfolio Dividends: ", self.portfolio_div_annual)
+        print("Average Portfolio Yield: ", self.ave_yield)
+        print("Average Portfolio YoC: ", self.ave_yoc)
+        print("\n===========\t===========\t===========\t===========\t===========\t\n")
+        # print(self.df.info())
+
+    def export(self):
+        # get current working directory
+        cwd = os.getcwd()
+        export_path = cwd + "/personal/portfolio.csv"
+        self.df.to_csv(export_path)
 
 
 class Stocks:
@@ -280,16 +407,17 @@ class Stocks:
         self.sort = sort_column
         self.df_all = pd.concat(data_list, axis=1)
         self.years = years
-        cols = ["Allocations", "Ave Annualized Div Perf", "2Y Ave Yield",
+        cols = ["Allocations", "Ave Annualized Div Perf", "Yield",
                 "Ave Div Growth"]
         self.df = self.df_all.loc[:, cols]
         self.col_mod = np.arange(0, len(cols), dtype=int)
-        self.analysis()
+        self.df.rename_axis("Symbols", inplace=True)
+        self.analyze()
 
-    def analysis(self):
-        # column index value to 4y ave yield
-        col_yield = self.df.columns.get_loc("2Y Ave Yield")
-        col_yield_str = "2Y Ave Yield"
+    def analyze(self):
+        # column index value to yield
+        col_yield = self.df.columns.get_loc("Yield")
+        col_yield_str = "Yield"
         yield_ = self.df.iloc[:, col_yield]
         div_grow = self.df.iloc[:, col_yield+1]
 
@@ -367,15 +495,17 @@ class Stocks:
                                self.df.columns[column_list[j]]].apply(f2p)
             self.df.iloc[:, column_list[j]] = edit
 
-    def present(self):
+    def cleanup(self):
         self.f2p_data(self.col_mod)
         self.f2p_data(self.col_add)
+
+    def present(self):
         self.portfolio_yield = f2p(self.portfolio_yield)
         print(self.df)
         print("\n===========\t===========\t===========\t===========\t===========\t\n")
         print("Average Portfolio Yield: ", self.portfolio_yield)
         print("\n===========\t===========\t===========\t===========\t===========\t\n")
-        print(self.df.info())
+        # print(self.df.info())
 
     def export(self):
         # get current working directory
@@ -386,7 +516,7 @@ class Stocks:
 
 if __name__ == "__main__":
     # set max columns to view in dataframe
-    pd.set_option("display.max_columns", 11)
+    pd.set_option("display.max_columns", 12)
     # get current working directory
     cwd = os.getcwd()
 
@@ -409,14 +539,18 @@ if __name__ == "__main__":
     # dividends analysis
     div = Dividends(path_div, columns_div, del_columns_div, ignore_list)
 
-    # holding info
-    path_hold = cwd + "/personal/m1.csv"
-    hold = Holdings(path_hold)
-    print(hold.df)
-
     data = [perf.df, div.df]
-    cols = ["Ave Annualized Div Perf", "2Y Ave Yield", "Ave Div Growth", ]
+    cols = ["Ave Annualized Div Perf", "Yield", "Ave Div Growth"]
     # stocks = Stocks(data, ignore_symbols=ignore_list, sort_column=cols[1])
     stocks = Stocks(data, ignore_symbols=ignore_list)
-    # stocks.present()
-    # stocks.export()
+    stocks.cleanup()
+    stocks.present()
+    stocks.export()
+
+    # holding info
+    path_hold = cwd + "/personal/m1.csv"
+    del_columns_hold = ["Name", "Unrealized Gain",
+                        "Unrealized Gain %", "Value"]
+    hold = Holdings(path_hold, div.df, del_columns_hold)
+    hold.cleanup()
+    hold.present()
